a4e0f634fac85be24677f9ce6c5d585cae1d18e3
meiXuP <otroom101@gmail.com>
2025-07-22 15:35:16 +0530

    Update script.js

--- login/script.js
+++ login/script.js
@@ -8,7 +8,7 @@ if (loginForm) {
         const email = loginForm.querySelector("#email").value;
         const password = loginForm.querySelector("#password").value;
 
-        const response = await fetch(`${ngrok}/login`, {
+        const response = await fetch(`https://9696eb1c37bc.ngrok-free.app/login`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             credentials: "include",  // 👈 Required if sending cookies
