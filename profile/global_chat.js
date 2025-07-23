914457a356a371d1fa6ad30ff68faf37ee3ddda3
meiXuP <otroom101@gmail.com>
2025-07-24 01:53:41 +0530

    Update global_chat.js

--- profile/global_chat.js
+++ profile/global_chat.js
@@ -71,7 +71,7 @@ function loadOnlineUsers() {
                 const userDiv = document.createElement("div");
                 userDiv.className = "user-info";
                 userDiv.innerHTML = `
-                    <img src=`${profilePic}` alt="Profile Picture" />
+                    <img `${profilePic}`/>
                     <h3>${username}</h3>
                     <div class="context-menu">
                         <button onclick="viewUser('${username}')">View</button>
